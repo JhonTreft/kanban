7d60360b7dc07ac458255ded5c4a0b2c5540a481
JhonTreft <jonz37133@gmail.com>
2023-06-02 12:49:13 -0500

    uploads files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainViewComponent } from './pages/main-view/main-view.component';
+import {FormViewComponent} from './pages/form-view/form-view.component'
+const routes: Routes = [
+  {
+    path:'',
+    component: MainViewComponent,
 
-const routes: Routes = [];
+  }
+  ,{
+    path:'notes',
+    component:FormViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainViewComponent } from './pages/main-view/main-view.component';
+
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { FormViewComponent } from './pages/form-view/form-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent,
+    FormViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
